a9d297ba567458d1daf880c4b403f1241e25a1c5
mehdi-norouzi <mehdeenoroozi@gmail.com>
2025-02-23 01:10:22 +0330

    remove freertos

--- src/globals/version.h
+++ src/globals/version.h
@@ -1,6 +1,6 @@
-// fw version: v0.1-0-g619c
+// fw version: v0.1-1-g5500
  #include <stdint.h>
  const uint8_t gGIT_VERSION_MAJOR = 0;
  const uint8_t gGIT_VERSION_MINOR = 1;
- const uint8_t gGIT_VERSION_PATCH = 0;
- #define gGIT_VERSION_HASH "g619c"
+ const uint8_t gGIT_VERSION_PATCH = 1;
+ #define gGIT_VERSION_HASH "g5500"
